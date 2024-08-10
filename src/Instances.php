72b1a7f532ca8522eab3fa2cda46fde15d1d2a11
= <inman.turbo@gmail.com>
2024-08-09 20:30:50 -0400

    “ It is never too late to be what you might have been. ”   — George Eliot

--- src/Instances.php
+++ src/Instances.php
@@ -2,4 +2,67 @@
 
 namespace Inmanturbo\Instances;
 
-class Instances {}
+use Inmanturbo\Modelware\Facades\Modelware;
+
+class Instances 
+{
+    public bool $disabled = false;
+
+    public function isDisabled(): bool
+    {
+        return $this->disabled;
+    }
+
+    public function disable(): void
+    {
+        $this->disabled = true;
+    }
+
+    public function enable(): void
+    {
+        $this->disabled = false;
+    }
+
+    public function bootListeners(): void
+    {
+        if ($this->isDisabled()) {
+            return;
+        }
+
+        if(config('instances.disabled', false)) {
+            return;
+        }
+
+        $this->listenForCreatingEvents();
+        $this->listenForUpdatingEvents();
+        $this->listenForDeletingEvents();
+    }
+
+    public function listenForCreatingEvents(): void
+    {
+        $this->listen('creating', [
+            // todo: add your pipes here
+        ]);
+    }
+
+    public function listenForUpdatingEvents(): void
+    {
+        $this->listen('updating', [
+            // todo: add your pipes here
+        ]);
+    }
+
+    public function listenForDeletingEvents(): void
+    {
+        $this->listen('deleting', [
+            // todo: add your pipes here
+        ]);
+    }
+
+    public function listen(string $event, array $pipes): void
+    {
+        Modelware::add($event, $pipes, prefix: 'instances');
+    }
+
+}
+
