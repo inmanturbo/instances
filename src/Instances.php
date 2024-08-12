b067441133174fb80f88c9b3a204664fd54598e4
= <inman.turbo@gmail.com>
2024-08-12 10:12:36 -0400

    “ I have not failed. I've just found 10,000 ways that won't work. ”   — Thomas Edison

--- src/Instances.php
+++ src/Instances.php
@@ -111,8 +111,6 @@ class Instances
             GetAttributes::class,
             CheckIfInstanceExists::class,
             StoreNewInstance::class,
-            FilterValues::class,
-            FillModel::class,
         ]);
     }
 
